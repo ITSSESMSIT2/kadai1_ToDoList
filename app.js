3739ad7092e32d6a435310d6d4a0be5a29ca64c4
mrnokmt <marin.okamoto@persol.co.jp>
2026-08-18 16:32:37 +0900

    第三回PRメイン要素。ローカル日付修正、配列空格納防止とそれに伴うREADME追記

--- app.js
+++ app.js
@@ -3,16 +3,22 @@ const ul = document.querySelector("#taskContainer");
 const taskList = [];
 
 const day = new Date();
-const japanDay = day.toISOString().replace("Z", "+09:00");
-// 確認用　console.log(japanDay);
-const sliceDay = japanDay.slice(0, 10);
-const defaultDay = document.querySelector("#taskLimit");
-defaultDay.value = `${sliceDay}`;
+const year = day.getFullYear();
+console.log(year);
+const monthNum = String(day.getMonth() + 1);
+const month = monthNum.padStart(2, "0");
+const dateNum = String(day.getDate());
+const date = dateNum.padStart(2, "0");
+const defaultDay = `${year}-${month}-${date}`;
+
+const setDefaultDay = document.querySelector("#taskLimit");
+setDefaultDay.value = defaultDay;
 
 let taskName = form.elements.taskName;
 let taskLimit = form.elements.taskLimit;
 let taskStatus = form.elements.taskStatus;
 let taskId = 0;
+
 // 今後、余力があれば実装したい要素。タスク進行ステータスの種類ごとの数を動的に表示
 const showStatusNams = document.querySelector("#showStatusNams");
 
@@ -20,21 +26,28 @@ const showStatusNams = document.querySelector("#showStatusNams");
 form.addEventListener("submit", function (e) {
   //submitのデフォルト処理（ページ遷移）を阻止
   e.preventDefault();
-  // submitが押された時点での情報を取得したいのでここ
-  taskId++;
-  const task = {
-    name: taskName.value,
-    limit: taskLimit.value,
-    status: taskStatus.value,
-    id: taskId,
-  };
-  taskList.push(task);
-  // 確認用　console.log(taskList);
-  addList(task);
-  // 入力欄を初期化
-  taskName.value = "";
-  taskLimit.value = `${sliceDay}`;
-  taskStatus.value = "0";
+  // ここで入力欄の空欄を確認。→配列に空が格納されるのを防止する
+  const name = taskName.value.trim();
+  if (name === "" || name === null) {
+    alert("タスク名を入力してください");
+    // ここでreturnを用いることで、入力値を保持したままにできる。
+    return;
+  } else {
+    taskId++;
+    const task = {
+      name: taskName.value,
+      limit: taskLimit.value,
+      status: taskStatus.value,
+      id: taskId,
+    };
+    taskList.push(task);
+    // 確認用　console.log(taskList);
+    addList(task);
+    // 入力欄を初期化
+    taskName.value = "";
+    taskLimit.value = defaultDay;
+    taskStatus.value = "0";
+  }
 });
 
 // 一覧に行を加えるための関数を作成
@@ -69,36 +82,29 @@ const addList = function (task) {
   deleteImg.style.pointerEvents = "none";
   deleteButton.append(deleteImg);
 
-  // nameが空文字もしくはnullのとき
-  if (task.name === "" || task.name === null) {
-    alert("タスク名を入力してください");
-    // それ以外（＝タスク名が入力されている）のとき、入力されたものをliに追加
-  } else {
-    // CSSで整理したいため、span要素をそれぞれに作成し、その中にオブジェクトの内容を格納
-    const nameSpan = document.createElement("span");
-    nameSpan.id = "nameSpan";
-    nameSpan.innerText = `${task.name}`;
-    li.append(nameSpan);
-    const limitSpan = document.createElement("span");
-    limitSpan.id = "limitSpan";
-    const taskLimitView = task.limit.replaceAll("-", "/");
-    limitSpan.innerText = taskLimitView;
-    li.append(limitSpan);
-    // セレクトボタンの中にもオブジェクトの内容を反映
-    statusSelect.value = task.status;
-    li.append(statusSelect);
-    li.append(deleteButton);
-    // 検索のため、liの中にidも格納するが非表示にしておく。
-    const idSpan = document.createElement("span");
-    idSpan.id = "idSpan";
-    idSpan.innerText = `${task.id}`;
-    li.append(idSpan);
-    idSpan.style.display = "none";
-    // 親要素ulの中にliを格納
-    ul.append(li);
-  }
+  // CSSで整理したいため、span要素をそれぞれに作成し、その中にオブジェクトの内容を格納
+  const nameSpan = document.createElement("span");
+  nameSpan.id = "nameSpan";
+  nameSpan.innerText = `${task.name}`;
+  li.append(nameSpan);
+  const limitSpan = document.createElement("span");
+  limitSpan.id = "limitSpan";
+  const taskLimitView = task.limit.replaceAll("-", "/");
+  limitSpan.innerText = taskLimitView;
+  li.append(limitSpan);
+  // セレクトボタンの中にもオブジェクトの内容を反映
+  statusSelect.value = task.status;
+  li.append(statusSelect);
+  li.append(deleteButton);
+  // 検索のため、liの中にidも格納するが非表示にしておく。
+  const idSpan = document.createElement("span");
+  idSpan.id = "idSpan";
+  idSpan.innerText = `${task.id}`;
+  li.append(idSpan);
+  idSpan.style.display = "none";
+  // 親要素ulの中にliを格納
+  ul.append(li);
 };
-
 // 画像がクリックされたときに、同じ欄の中にあるToDoを削除
 ul.addEventListener("click", function (ev) {
   // ボタン（画像はマウスイベントを無視）を押すと、親要素が削除されるように設定
@@ -110,8 +116,8 @@ ul.addEventListener("click", function (ev) {
     // 配列内の要素に、クリックされたliのidと一致するものがあるかを確認する関数を用意
     const checkId = (element) => element.id === parentIdNumber;
     // 初めにtrueを返す要素のインデックスを渡す、findIndexを利用
-    const deliteTargetIndex = Number(taskList.findIndex(checkId));
-    if (deliteTargetIndex !== -1 && deliteTargetIndex !== null) {
+    const deliteTargetIndex = taskList.findIndex(checkId);
+    if (deliteTargetIndex !== -1) {
       // 配列から、一致するインデックスの要素一項目のみを削除
       taskList.splice(deliteTargetIndex, 1);
       //画面上からも削除。buttonからもっとも近いli要素を指定する。
@@ -136,8 +142,8 @@ ul.addEventListener("change", function (eve) {
     // 配列内のid要素と、idSpanの中の数字が一致する要素を探す関数を用意
     const checkId = (element) => element.id === parentIdNumber;
     // findIndex(配列内の要素で、初めに一致するものを返す)を利用し検索、インデックスを数字で返してもらう
-    const changeTargetIndex = Number(taskList.findIndex(checkId));
-    if (changeTargetIndex !== -1 && changeTargetIndex !== null) {
+    const changeTargetIndex = taskList.findIndex(checkId);
+    if (changeTargetIndex !== -1) {
       // .changeStatusはliのselect要素につけたclass名
       const changeStatusValue = parent.querySelector(".changeStatus").value;
       taskList[changeTargetIndex].status = changeStatusValue;
